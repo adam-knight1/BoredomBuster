fe5cce37c25db5a7463a65121b335a35a823521e
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-02-21 23:58:04 -0500

    Initial commit

---
+++ src/main/java/org/informationblitz/FactService.java
@@ -0,0 +1,42 @@
+package org.informationblitz;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class FactService {
+
+    public void getFactsFromAPI() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("How many facts would you like to see, up to 3");
+            int numOfFacts = scanner.nextInt();
+
+            URL url = new URL("https://api.api-ninjas.com/v1/facts?limit="+numOfFacts);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("X-Api-key", "iksW+ahtgKdZfdUHvWXGXA==Tv4PHnyj3CpuUHQP");
+            connection.setRequestProperty("accept", "application/json");
+            InputStream responseStream = connection.getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(responseStream);
+            int factCounter = 1 ;
+
+            if (root.isArray()) {
+                for (JsonNode node : root) {
+                    String fact = node.path("fact").asText();
+                    System.out.println("Fact number: " + factCounter + ": " + fact);
+                    factCounter++;
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
---
+++ src/main/java/org/informationblitz/TriviaDTO.java
@@ -0,0 +1,21 @@
+package org.informationblitz;
+
+public class TriviaDTO {
+    public String triviaQuestion;
+    private String category;
+    private String question;
+    private String answer;
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+}
