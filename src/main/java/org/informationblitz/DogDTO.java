3f5588964dd66ff8579afcbf497bd2f8bb7db206
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-02-23 00:32:19 -0500

    Added getters to dogDTO and print statements for parameters

---
+++ src/main/java/org/informationblitz/DogDTO.java
@@ -0,0 +1,49 @@
+package org.informationblitz;
+
+public class DogDTO {
+    private String name;
+    private String imageLink;
+    private int goodWithChildren;
+    private int goodWithOtherDogs;
+    private int shedding;
+    private int energy;
+    private int trainability;
+    private int minLifeExpectancy;
+    private int maxLifeExpectancy;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public int getGoodWithChildren() {
+        return goodWithChildren;
+    }
+
+    public int getGoodWithOtherDogs() {
+        return goodWithOtherDogs;
+    }
+
+    public int getShedding() {
+        return shedding;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public int getTrainability() {
+        return trainability;
+    }
+
+    public int getMinLifeExpectancy() {
+        return minLifeExpectancy;
+    }
+
+    public int getMaxLifeExpectancy() {
+        return maxLifeExpectancy;
+    }
+}
