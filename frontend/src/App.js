0afac95cd1457f7b66e57bf45ec78c363abbad76
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-02-23 01:34:16 -0500

    added app.js React logic for dogs, implementing DogController to route requests to front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  // this state to hold the dog information, set to null init
+  const [dog, setDog] = useState(null);
+
+  // after the component mounts, this fetches the dog data
+  useEffect(() => {
+    fetch('/api/dogs?name=golden retriever') //GET, need to put correct url in
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // log to see the structure
+        setDog(data); // idk if backend will return correctly yet
+      })
+      .catch(error => console.error('There was an error!', error));
+  }, []); //runs once after initial render?
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {dog ? (
+          <div>
+            <h1>{dog.name}</h1> {/* dog name*/}
+            {/* the other stuff */}
+          </div>
+        ) : (
+          <p>Loading...</p> //will integrate more detailed load message.
+        )}
       </header>
     </div>
   );
