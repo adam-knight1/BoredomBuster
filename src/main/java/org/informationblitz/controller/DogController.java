e333fcaa0729be03cbc01320c35cacb1eb6e1b38
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-02-23 01:35:58 -0500

    implementing DogController logic

--- src/main/java/org/informationblitz/controller/DogController.java
+++ src/main/java/org/informationblitz/controller/DogController.java
@@ -1,4 +1,13 @@
 package org.informationblitz.controller;
 
+import org.informationblitz.service.DogService;
+
 public class DogController {
+    private final DogService dogService;
+
+    public DogController(DogService dogService){
+        this.dogService = dogService;
+    }
+
+    @GetMapping
 }
