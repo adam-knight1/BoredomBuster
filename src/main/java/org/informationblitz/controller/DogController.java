4fe0cee15d4d1cc76af563c5195e63c871b7bfe8
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-02-23 01:54:26 -0500

    updated gradle build with new spring boot dependencies, added DogController

--- src/main/java/org/informationblitz/controller/DogController.java
+++ src/main/java/org/informationblitz/controller/DogController.java
@@ -1,18 +1,25 @@
 package org.informationblitz.controller;
 
 import org.informationblitz.service.DogService;
-@Rest
-@RequestMapping("/api")
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dogs")
 public class DogController {
     private final DogService dogService;
 
-    public DogController(DogService dogService){
+    public DogController(DogService dogService) {
         this.dogService = dogService;
     }
 
+
     @GetMapping("/dog")
+    Public ResponseEn
+
 
 
 
     }
-}
+}*/
