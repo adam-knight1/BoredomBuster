2be56bdad44dc1c21e2937f7f06e7094a7a14cf1
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-02-23 02:03:55 -0500

    Refactoring getDogInfo controller method

--- src/main/java/org/informationblitz/controller/DogController.java
+++ src/main/java/org/informationblitz/controller/DogController.java
@@ -1,25 +1,35 @@
 package org.informationblitz.controller;
 
+import org.informationblitz.dto.DogDTO;
 import org.informationblitz.service.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/dogs")
 public class DogController {
-    private final DogService dogService;
+
+    @Autowired
+    private DogService dogService;
 
     public DogController(DogService dogService) {
         this.dogService = dogService;
     }
 
-
     @GetMapping("/dog")
-    Public ResponseEn
-
-
-
-
+    public ResponseEntity<DogDTO> getDogInfo(@RequestParam String breedName) throws IOException {
+        try {
+            DogDTO dogInfo = dogService.getDogInfoFromAPI(breedName);
+            return ResponseEntity.ok(dogInfo);
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
-}*/
+
+}
