2c4ac29ef87e54d479af706c3791229943698f26
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-02-25 01:33:39 -0500

    Added service injection in WeatherController constructor

--- src/main/java/org/informationblitz/controller/WeatherController.java
+++ src/main/java/org/informationblitz/controller/WeatherController.java
@@ -1,5 +1,8 @@
 package org.informationblitz.controller;
 
+import org.informationblitz.service.DogService;
+import org.informationblitz.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/weather")
 public class WeatherController {
 
+    @Autowired
+    private WeatherService weatherService;
+
+    public WeatherController (WeatherService weatherService){
+        this.weatherService = weatherService;
+    }
 }
