e4852e03e8355ecb467113229ce7ca7693d384bd
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-02-24 20:01:04 -0500

    Added home.js with placeholder logic for site entry

---
+++ frontend/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Boredom Buster App</h1>
+      <p>This is the home page. Navigate to different pages using the navbar.</p>
+    </div>
+  );
+}
+
+export default Home;
