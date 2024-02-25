b2a3f31e14dc5939589ff22ebec16e8c7618b029
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-02-25 01:31:49 -0500

    Added webconfig for CORS between frontend, added WeatherController class

---
+++ src/main/java/org/informationblitz/controller/WeatherController.java
@@ -0,0 +1,10 @@
+package org.informationblitz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/weather")
+public class WeatherController {
+
+}
