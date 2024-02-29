171f1d80410759c04e413f86880df8aa5bcb2c5a
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-02-28 19:47:59 -0500

    refactoring weather service method for spring

--- src/main/java/org/informationblitz/controller/WeatherController.java
+++ src/main/java/org/informationblitz/controller/WeatherController.java
@@ -1,10 +1,17 @@
 package org.informationblitz.controller;
 
+import org.informationblitz.dto.DogDTO;
+import org.informationblitz.dto.WeatherDTO;
 import org.informationblitz.service.DogService;
 import org.informationblitz.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/weather")
@@ -15,5 +22,15 @@ public class WeatherController {
 
     public WeatherController (WeatherService weatherService){
         this.weatherService = weatherService;
+    }
+
+    @GetMapping("/forecast")
+    public ResponseEntity<WeatherDTO> getWeatherInfo(@RequestParam String zipCode) throws IOException { //string or int?
+        try {
+            WeatherDTO weatherInfo = weatherService.getWeatherFromAPI();
+            return ResponseEntity.ok(weatherInfo);
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
