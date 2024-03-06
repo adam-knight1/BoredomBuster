ba0362d7c2322d92ccbfc0d8d365cc25c9c38174
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-06 15:56:11 -0500

    basic implementation of getTriviaQuestion in TriviaController

--- src/main/java/org/informationblitz/controller/TriviaController.java
+++ src/main/java/org/informationblitz/controller/TriviaController.java
@@ -1,23 +1,30 @@
 package org.informationblitz.controller;
 
 import org.informationblitz.dto.TriviaDTO;
+import org.informationblitz.service.TriviaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/trivia")
 public class TriviaController {
 
+    @Autowired
+    private TriviaService triviaService;
+
     @GetMapping("/dogs")
-    public ResponseEntity<TriviaDTO> getTriviaQuestion(@RequestParam String category){
-
-        TriviaDTO trivia = TriviaService.getTrivia(category);
-
+    public ResponseEntity<TriviaDTO> getTriviaQuestion(@RequestParam String category) {
+        try {
+            TriviaDTO trivia = TriviaService.getTrivia(category);
+            return ResponseEntity.ok(trivia);
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(e.getStatus()).body(null);
+        }
     }
-
-
 }
