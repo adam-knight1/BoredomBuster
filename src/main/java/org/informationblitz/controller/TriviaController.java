02371b42b36df1ba19ce384d96905955b2478a8a
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-06 15:52:33 -0500

    implementing getTriviaQuestion in TriviaController

--- src/main/java/org/informationblitz/controller/TriviaController.java
+++ src/main/java/org/informationblitz/controller/TriviaController.java
@@ -1,11 +1,23 @@
 package org.informationblitz.controller;
 
+import org.informationblitz.dto.TriviaDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/trivia")
 public class TriviaController {
+
+    @GetMapping("/dogs")
+    public ResponseEntity<TriviaDTO> getTriviaQuestion(@RequestParam String category){
+
+        TriviaDTO trivia = TriviaService.getTrivia(category);
+
+    }
+
 
 }
--- src/main/java/org/informationblitz/service/TriviaService.java
+++ src/main/java/org/informationblitz/service/TriviaService.java
@@ -81,7 +81,6 @@ public class TriviaService {
                 triviaQuestion = ("Failed to fetch trivia");
                 e.printStackTrace();
             }
-
         }
     }
 }
