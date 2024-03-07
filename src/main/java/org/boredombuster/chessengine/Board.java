ebdbf80a0309e4f74d02bdcf143ea17fc46434df
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-07 17:43:08 -0500

    implementing setupBoard and Pawn class extending Piece)

--- src/main/java/org/boredombuster/chessengine/Board.java
+++ src/main/java/org/boredombuster/chessengine/Board.java
@@ -4,9 +4,13 @@ public class Board {
     private Piece[][] board;
     private Boolean whiteTurn = true;
 
+    public Board(){
+        this.board = new Piece[8][8];
+    }
+
     private void setupBoard() {
         for (int i = 0; i < 8; i++) {
-
+            board[1][i] = new pawn(1,i,true);
         }
     }
 }
---
+++ src/main/java/org/boredombuster/chessengine/piecelogic/Pawn.java
@@ -0,0 +1,7 @@
+package org.boredombuster.chessengine.piecelogic;
+
+import org.boredombuster.chessengine.Piece;
+
+public class Pawn extends Piece {
+
+}
