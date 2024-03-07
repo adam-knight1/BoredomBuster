c07923cb009ff30d06f453a241fc72fc68c0c442
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-07 17:44:52 -0500

    added construtor to Piece

--- src/main/java/org/boredombuster/chessengine/piecelogic/Pawn.java
+++ src/main/java/org/boredombuster/chessengine/piecelogic/Pawn.java
@@ -4,4 +4,8 @@ import org.boredombuster.chessengine.Piece;
 
 public class Pawn extends Piece {
 
+    public Pawn(int positionX, int positionY, boolean isWhite){
+        super(positionX,positionY,isWhite);
+    }
+
 }
