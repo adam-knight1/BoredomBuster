6582cab27cb75ed146fbae96158d7741a5d7ba06
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-07 17:16:11 -0500

    coding Board.setupBoard to position pieces

--- src/main/java/org/boredombuster/chessengine/Board.java
+++ src/main/java/org/boredombuster/chessengine/Board.java
@@ -2,4 +2,11 @@ package org.boredombuster.chessengine;
 
 public class Board {
     private Piece[][] board;
+    private Boolean whiteTurn = true;
+
+    private void setupBoard() {
+        for (int i = 0; i < 8; i++) {
+
+        }
+    }
 }
