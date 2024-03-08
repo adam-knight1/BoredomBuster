fefc6398fce47fe4b6dd78bfaf9b4ccaedd696d9
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-07 19:49:26 -0500

    Added method headers for isValidMove and isWhite in Piece abstract class

--- src/main/java/org/boredombuster/chessengine/Piece.java
+++ src/main/java/org/boredombuster/chessengine/Piece.java
@@ -7,6 +7,14 @@ public abstract class Piece {
 
 
     public Piece(int positionX, int positionY, boolean isWhite) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.isWhite = isWhite;
+    }
 
+    public abstract boolean isValidMove(Board board, int destinationX, int destinationY);
+
+    public boolean isWhite() {
+        return isWhite;
     }
 }
