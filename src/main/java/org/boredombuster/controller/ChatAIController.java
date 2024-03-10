8c405ac5ca475ce99ed7145f559f1e3462c2f93b
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-10 17:23:51 -0400

    Beginning addition of AI chatboat

--- src/main/java/org/boredombuster/controller/ChatAIController.java
+++ src/main/java/org/boredombuster/controller/ChatAIController.java
@@ -1,4 +1,13 @@
 package org.boredombuster.controller;
 
+import org.boredombuster.dto.ChatMessageDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatAIController {
+
+    public ResponseEntity<String> getChatResponse(String prompt) {
+
+    }
 }
