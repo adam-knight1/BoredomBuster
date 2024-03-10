c50eda5bb2fc0ee85e74e732e844232d989be1d3
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-10 17:33:21 -0400

    Added flux dependency to use webclient instead of rest template for non-blocking features

--- src/main/java/org/boredombuster/service/ChatAIService.java
+++ src/main/java/org/boredombuster/service/ChatAIService.java
@@ -1,7 +1,19 @@
 package org.boredombuster.service;
 
+import org.springframework.web.reactive.function.client.WebClient;
+
 public class ChatAIService {
     private String apiKey = System.getenv("OPENAI_API_KEY");
 
-    private String getAIResponse()
+    private final WebClient webClient;
+
+    public ChatAIService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://api.openai.com/v1").build();
+    }
+
+    public String getResponseFromAI(String prompt){
+
+        return "";
+
+    }
 }
