71761a82e2acf7c67218a1a55f1d7b1c3788a30c
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-10 18:08:10 -0400

    Completed initial implementaiton of chatAIControlle.getChatResponse

--- src/main/java/org/boredombuster/dto/ChatMessageDTO.java
+++ src/main/java/org/boredombuster/dto/ChatMessageDTO.java
@@ -8,4 +8,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class ChatMessageDTO {
     @JsonProperty("message")
     private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
