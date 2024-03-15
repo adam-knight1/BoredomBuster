bea4b7f2419b6d2f770e0e9239fc47444b38e682
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-15 16:55:10 -0400

    Added basic implementation of makeMove controller endpopint, calling setupBoard and calculateBestMove

--- src/main/java/org/boredombuster/controller/ChessController.java
+++ src/main/java/org/boredombuster/controller/ChessController.java
@@ -2,9 +2,14 @@ package org.boredombuster.controller;
 
 import org.boredombuster.chessengine.ChessEngineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/chess")
@@ -13,5 +18,16 @@ public class ChessController {
     @Autowired
     public ChessController(ChessEngineService chessEngineService) {
         this.chessEngineService = chessEngineService;
+    }
+
+    @PostMapping("/start")
+    public ResponseEntity<String> makeMove(@RequestParam String moves, @RequestParam int depth) {
+        try {
+            chessEngineService.setupBoard(moves);
+            String bestMove = chessEngineService.calculateBestMove(depth);
+            return ResponseEntity.ok("Best move is: " + bestMove);
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().body("failed to calculate move" + e.getMessage());
+        }
     }
 }
