224413521499cd4d58b5e7ea924bddff7309e0d6
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-15 17:01:11 -0400

    Added basic startGame endpoint, added spaces after badRequest responses for readability

--- src/main/java/org/boredombuster/controller/ChessController.java
+++ src/main/java/org/boredombuster/controller/ChessController.java
@@ -3,7 +3,6 @@ package org.boredombuster.controller;
 import org.boredombuster.chessengine.ChessEngineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,14 +19,34 @@ public class ChessController {
         this.chessEngineService = chessEngineService;
     }
 
+    /** starts the chess engine by calling the method in ChessEngineService
+     *
+     * @return validated for successful start or failed to start game
+     */
     @PostMapping("/start")
+    public ResponseEntity<String> startGame() {
+        try {
+            chessEngineService.startEngine();
+            return ResponseEntity.ok("Chess engine started success");
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().body("failed to start game: " + e.getMessage());
+        }
+    }
+
+    /** Calls setup board with current positioning and calls best move method in service with depth
+     * Depth = moves that SF is looked ahead for analysis of best move
+     * @param moves
+     * @param depth
+     * @return Response with best more or failed to calculate move response
+     */
+    @PostMapping("/move")
     public ResponseEntity<String> makeMove(@RequestParam String moves, @RequestParam int depth) {
         try {
             chessEngineService.setupBoard(moves);
             String bestMove = chessEngineService.calculateBestMove(depth);
             return ResponseEntity.ok("Best move is: " + bestMove);
         } catch (IOException e) {
-            return ResponseEntity.badRequest().body("failed to calculate move" + e.getMessage());
+            return ResponseEntity.badRequest().body("failed to calculate move: " + e.getMessage());
         }
     }
 }
