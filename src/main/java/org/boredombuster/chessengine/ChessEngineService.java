64f3bf95bdaa6e8f4c56f7a652bf4edc43e5936e
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-15 14:51:59 -0400

    Beginning implementation of ChessEngineService to interact with stockfish.  Added engineProcess, buffered reader and writer

---
+++ src/main/java/org/boredombuster/chessengine/ChessEngineService.java
@@ -0,0 +1,12 @@
+package org.boredombuster.chessengine;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+//This class will help to integrate the stockfish chess engine into the app
+public class ChessEngineService {
+    private Process engineProcess;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+
+}
