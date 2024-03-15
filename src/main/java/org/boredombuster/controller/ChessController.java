29747487019c29faad6a185061f7186d598dc909
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-15 16:49:36 -0400

    implementing ChessController, adding constructor that initializes ChessEngineService

--- src/main/java/org/boredombuster/controller/ChessController.java
+++ src/main/java/org/boredombuster/controller/ChessController.java
@@ -1,4 +1,17 @@
 package org.boredombuster.controller;
 
+import org.boredombuster.chessengine.ChessEngineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chess")
 public class ChessController {
+    private final ChessEngineService chessEngineService;
+    @Autowired
+    public ChessController(ChessEngineService chessEngineService) {
+        this.chessEngineService = chessEngineService;
+    }
 }
