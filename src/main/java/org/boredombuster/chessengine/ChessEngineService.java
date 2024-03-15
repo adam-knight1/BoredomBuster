2f1df03657eba854f03a9481e615af300b6a27b7
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-15 15:26:46 -0400

    Adding annotations for ChessEngineService, set stockfish path in app.properties

--- src/main/java/org/boredombuster/chessengine/ChessEngineService.java
+++ src/main/java/org/boredombuster/chessengine/ChessEngineService.java
@@ -1,12 +1,28 @@
 package org.boredombuster.chessengine;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
+import org.springframework.stereotype.Service;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 
 //This class will help to integrate the stockfish chess engine into the app
+@Service
 public class ChessEngineService {
+
+    //initializing stockfish and UCI components
     private Process engineProcess;
     private BufferedReader reader;
     private BufferedWriter writer;
+    @Value("${stockfish.path}")
+    private String stockfishPath;
 
+
+    public void startEngine() throws IOException {
+        ProcessBuilder builder = new ProcessBuilder("/opt/homebrew/bin/stockfish"
+);
+
+    }
 }
